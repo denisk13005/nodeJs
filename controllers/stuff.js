4c143c400ce90b04c8b1ea6116f7d4763ec6b5a8
kirmann <k.dkle@hotmail.fr>
2022-05-31 15:33:20 +0200

    mise en place du middlawre auth

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -23,10 +23,10 @@ exports.createThing = (req, res, next) => {
     }
   );
 };
-
+// get pour un thing en particulier avec son id
 exports.getOneThing = (req, res, next) => {
   Thing.findOne({
-    _id: req.params.id
+    _id: req.params.id // id du thing à récupérer
   }).then(
     (thing) => {
       res.status(200).json(thing);
@@ -40,8 +40,9 @@ exports.getOneThing = (req, res, next) => {
   );
 };
 
+// put pour un thing en particulier avec son id
 exports.modifyThing = (req, res, next) => {
-  const thing = new Thing({
+  const thing = new Thing({ // on crée un nouvel objet de type Thing pour modifier le thing existant
     _id: req.params.id,
     title: req.body.title,
     description: req.body.description,
@@ -49,7 +50,7 @@ exports.modifyThing = (req, res, next) => {
     price: req.body.price,
     userId: req.body.userId
   });
-  Thing.updateOne({_id: req.params.id}, thing).then(
+  Thing.updateOne({_id: req.params.id}, thing).then( // on modifie le thing existant
     () => {
       res.status(201).json({
         message: 'Thing updated successfully!'
@@ -63,23 +64,37 @@ exports.modifyThing = (req, res, next) => {
     }
   );
 };
-
-exports.deleteThing = (req, res, next) => {
-  Thing.deleteOne({_id: req.params.id}).then(
-    () => {
-      res.status(200).json({
-        message: 'Deleted!'
-      });
+// delete pour un thing en particulier avec son id
+exports.deleteThing = (req, res, next) => { 
+  Thing.findOne({ _id: req.params.id }).then( // on récupère le thing à supprimer avec son id
+    (thing) => {
+      if (!thing) { // si le thing n'existe pas
+        res.status(404).json({
+          error: new Error('No such Thing!')
+        });
+      }
+      if (thing.userId !== req.auth.userId) { // si l'utilisateur n'est pas le propriétaire du thing
+        res.status(400).json({
+          error: new Error('Unauthorized request!')
+        });
+      }
+      Thing.deleteOne({ _id: req.params.id }).then( // on supprime le thing 
+        () => {
+          res.status(200).json({
+            message: 'Deleted!'
+          });
+        }
+      ).catch(
+        (error) => {
+          res.status(400).json({
+            error: error
+          });
+        }
+      );
     }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
+  )
 };
-
+ // get pour tous les things
 exports.getAllStuff = (req, res, next) => {
   Thing.find().then(
     (things) => {
--- middleware/auth.js
+++ middleware/auth.js
@@ -4,8 +4,9 @@ module.exports = (req, res, next) => {
 
   try {
     const token = req.headers.authorization.split(' ')[1];//on récupère le token dans le header
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');//on vérifie le token avec le sel
+    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');//on décode le token avec la même clef de crypatage que pour le token d'authentification (on a défini cette clef dans le fichier controllers/user.js)
     const userId = decodedToken.userId;//on récupère l'id de l'utilisateur dans le token
+    req.auth = {userId} //on ajoute l'id de l'utilisateur dans la requête pour pouvoir l'utiliser dans les autres middlewares
     if (req.body.userId && req.body.userId !== userId) { //si l'id de l'utilisateur dans le token et celui dans le body ne sont pas identiques
       throw 'Invalid user ID'; //on lance une erreur
     } else {
